da59786d883cdffa4b606ccafabba51a3484b817
Bryan Shelton <bryan@rover.com>
2016-03-07 09:46:00 -0800

    Flask example

--- app/app.py
+++ app/app.py
@@ -1,15 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello 11'
-
-
-@app.route('/subpage/')
-def subpage():
-    return 'This is a subpage'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
